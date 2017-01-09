5a8e9d2a870c1cebd12cad8913bf5c2c18e089f3
hovhannesdavtyan94@gmail.com <hovhannesdavtyan94@gmail.com>
2017-01-09 19:21:12 +0400

    pug support ,

--- FootBallVideos/ClientApp/app/components/navmenu/navmenu.component.ts
+++ FootBallVideos/ClientApp/app/components/navmenu/navmenu.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'nav-menu',
-    template: require('./navmenu.component.html'),
+    template: require('./navmenu.component.pug'),
     styles: [require('./navmenu.component.css')]
 })
 export class NavMenuComponent {
