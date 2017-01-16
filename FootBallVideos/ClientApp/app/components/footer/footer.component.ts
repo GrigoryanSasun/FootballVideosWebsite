f4d4ab78ffe7d27409b3fd99a51f28caed4c8d85
LuizaKishoyan <luizakishoyan@gmail.com>
2017-01-16 11:17:32 +0400

    UI changes

---
+++ FootBallVideos/ClientApp/app/components/footer/footer.component.ts
@@ -0,0 +1,12 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'footer',
+    template: require('./footer.component.pug'),
+    styles: [require('./footer.component.css')]
+})
+export class FooterComponent {
+    public currentCount = 0;
+
+   
+}
