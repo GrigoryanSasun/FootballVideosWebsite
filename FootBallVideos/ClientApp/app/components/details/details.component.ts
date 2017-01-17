53a47b0b11f987efb99135108e1285b5d82dad9b
LuizaKishoyan <luizakishoyan@gmail.com>
2017-01-17 16:56:50 +0400

    Details page

---
+++ FootBallVideos/ClientApp/app/components/details/details.component.ts
@@ -0,0 +1,10 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'videodetails',
+    template: require('./details.component.pug'),
+    styles: [require('./details.component.css')]
+})
+export class DetailsComponent {
+
+}
