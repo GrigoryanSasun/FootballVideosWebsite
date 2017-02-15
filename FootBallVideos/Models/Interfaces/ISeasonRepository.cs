52b837cdbc1ff5a61402609023b7543aa5013807
hdavtyan <hovhannesdavtyan94@gmail.com>
2017-02-15 19:13:48 +0400

    bug fix

---
+++ FootBallVideos/Models/Interfaces/ISeasonRepository.cs
@@ -0,0 +1,18 @@
+﻿using FootBallVideos.ModelsData;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FootBallVideos.Models
+{
+    public interface ISeasonRepository
+    {
+        IEnumerable<Season> GetAll();
+        Season Find(int key);
+        Task<bool> Add(Season item);
+        bool Update(Season item);
+        bool Remove(int key);
+
+        Task<IEnumerable<Season>> GetAllAsync();
+        Task<Season> FindAsync(int key);
+    }
+}
