2ab9217e1192d3f4110ca1403fbd4e8c4ea207d9
hdavtyan <hovhannesdavtyan94@gmail.com>
2017-02-15 19:07:58 +0400

    ITeamSeasonTournamentMapRepository added

---
+++ FootBallVideos/Models/Interfaces/ITeamSeasonTournamentMapRepository.cs
@@ -0,0 +1,18 @@
+﻿using FootBallVideos.ModelsData;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FootBallVideos.Models
+{
+    public interface ITeamSeasonTournamentMapRepository
+    {
+        IEnumerable<TeamSeasonTournamentMap> GetAll();
+        TeamSeasonTournamentMap Find(int key);
+        Task<bool> Add(TeamSeasonTournamentMap item);
+        bool Remove(int key);
+        bool Update(TeamSeasonTournamentMap item);
+
+        Task<IEnumerable<TeamSeasonTournamentMap>> GetAllAsync();
+        Task<TeamSeasonTournamentMap> FindAsync(int key);
+    }
+}
