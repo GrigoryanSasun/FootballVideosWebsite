18d9c985d206090963f821cabc1d419791bc8f38
Sasun <sasungrig@gmail.com>
2017-04-21 17:35:22 +0400

    node.js support added

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-npm install && npm run build:prod && dotnet restore && ASPNETCORE_ENVIRONMENT=Production dotnet run
+npm run build:prod && dotnet restore && ASPNETCORE_ENVIRONMENT=Production dotnet run
