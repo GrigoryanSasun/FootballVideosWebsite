f4b8918dc29936825f64b3a5bb0cdd5c5eaa49cb
Sasun <sasungrig@gmail.com>
2017-04-21 17:14:22 +0400

    node.js version specified in .travis.yml

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-nvm install "$(jq -r '.engines.node' package.json)" && npm install && npm run build:prod && dotnet restore && ASPNETCORE_ENVIRONMENT=Production dotnet run
+npm install && npm run build:prod && dotnet restore && ASPNETCORE_ENVIRONMENT=Production dotnet run
