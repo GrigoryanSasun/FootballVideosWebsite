7557c6871730721ca6d3c1b5102a99ba303b6951
Sasun <sasungrig@gmail.com>
2017-04-25 16:50:35 +0400

    'message'

--- Program.cs
+++ Program.cs
@@ -15,6 +15,7 @@ namespace FootballVideosWebsite
                         .Build();
 
 
+
             var host = new WebHostBuilder()
                 .CaptureStartupErrors(true)
                 // .UseSetting("detailedErrors", "true")
